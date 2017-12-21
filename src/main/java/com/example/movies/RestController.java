e459a9d47ec3877dc8929fd36217e17a78b30a3c
Sergio Sanchez Colilla <sergio.sanchezc@altran.es>
2017-12-21 12:39:59 +0100

    Api configuration to bug fixes about origin access

--- src/main/java/com/example/movies/RestController.java
+++ src/main/java/com/example/movies/RestController.java
@@ -3,9 +3,11 @@ package com.example.movies;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -22,85 +24,66 @@ public class RestController {
 
 	//------DOWNLOADER METHODS
 	@RequestMapping("/searchMovie")
-	public ArrayList<Movie> searchMovie(@RequestParam(value = "name") String name, HttpServletResponse  response) throws MalformedURLException {
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+	@CrossOrigin
+	public ArrayList<Movie> searchMovie(@RequestParam(value = "name") String name) throws MalformedURLException {
 		return Downloader.searchMovie(name, Config.getDOMAIN());
 	}
-
+	
 	@RequestMapping("/getMovieInfo")
-	public Movie getMovieInfo(@RequestParam(value = "url") String url, HttpServletResponse  response) throws MalformedURLException {
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+	@CrossOrigin
+	public Movie getMovieInfo(@RequestParam(value = "url") String url) throws MalformedURLException {
 		return Downloader.getMovieInfo(url, Config.getDOMAIN());
 	}
 
 	@RequestMapping("/getMovie")
-	public Response getMovie(@RequestParam(value = "address") String address, HttpServletResponse  response) throws MalformedURLException {
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+	@CrossOrigin
+	public Response getMovie(@RequestParam(value = "address") String address) throws MalformedURLException {
 		return Downloader.getMovie(Config.getIPADDRESS(), address, Config.getDOMAIN());
+	}
+	
+	@RequestMapping("/availability")
+	@CrossOrigin
+	public Response availability() throws MalformedURLException {
+		Response response = new Response("availability", "200 OK - Server running");
+		return response ;
 	}
 
 	//------LIBRARY METHODS
 	@RequestMapping("/addFile")
-	public Response addFile(@RequestParam(value = "source") String source, HttpServletResponse  response) throws MalformedURLException {
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+	@CrossOrigin
+	public Response addFile(@RequestParam(value = "source") String source) throws MalformedURLException {
 		return Library.addFile(Config.getIPADDRESS(), source);
 	}
 
 	@RequestMapping("/getInfo")
-	public JSONArray getMovieInfo(HttpServletResponse  response) throws MalformedURLException {
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+	@CrossOrigin
+	public JSONArray getMovieInfo() throws MalformedURLException {
 		return Library.getInfo(Config.getIPADDRESS());
 	}
 
 	@RequestMapping("/optionFile")
-	public Response optionFile(@RequestParam(value = "hash") String hash, @RequestParam(value = "option") String option, HttpServletResponse  response) throws MalformedURLException {
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+	@CrossOrigin
+	public Response optionFile(@RequestParam(value = "hash") String hash, @RequestParam(value = "option") String option) throws MalformedURLException {
 		return Library.optionFile(Config.getIPADDRESS(), hash, option);
 	}
 
 	//------API METHODS
 	@RequestMapping("/getMovies")
-	public ArrayList<Movie> getMovies(@RequestParam(value = "busquedaURL") String busquedaURL, @RequestParam(value = "year") String year, HttpServletResponse  response) throws MalformedURLException {	
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+	@CrossOrigin
+	public ArrayList<Movie> getMovies(@RequestParam(value = "busquedaURL") String busquedaURL, @RequestParam(value = "year") String year) throws MalformedURLException {	
 		return MoviesAPI.getMovies(busquedaURL,year);
 	}
 	
 	@RequestMapping("/getTrailer")
-	public Response getTrailer(@RequestParam(value = "id") int id, HttpServletResponse  response) throws MalformedURLException {
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+	@CrossOrigin
+	public Response getTrailer(@RequestParam(value = "id") int id) throws MalformedURLException {
 		return MoviesAPI.getTrailer(id);
 	}
 	
 	@RequestMapping("/getCastList")
-	public ArrayList<Cast> getCastList(@RequestParam(value = "idPelicula") int idPelicula, HttpServletResponse  response) throws MalformedURLException {
+	public ArrayList<Cast> getCastList(@RequestParam(value = "idPelicula") int idPelicula, ServletResponse res) throws MalformedURLException {
+		HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
 		return MoviesAPI.getCastList(idPelicula);
 	}
 
